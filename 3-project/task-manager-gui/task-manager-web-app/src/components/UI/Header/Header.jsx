c150321dc87aa9554181ad5a0e5beb2023829007
AymenMarouani <marouani.aymen@gmail.com>
2019-05-08 21:51:23 +0200

    Display mock data on a table

---
+++ 3-project/task-manager-gui/task-manager-web-app/src/components/UI/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+export default function Header() {
+  return (
+    <AppBar position="static" color="default">
+      <Toolbar>
+        <IconButton color="inherit" aria-label="Menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" color="inherit">
+          Task Manager
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+}
